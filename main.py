48b0ea42b1ed6206d15bd1d788568787b54350a8
basavaraj2k <basavarajmp23@gmail.com>
2023-05-26 13:17:33 +0530

    Add API mapping

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!.")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def say_hello():
+    return "Hello, API!"
+
+if __name__ == "__main__":
+    app.run()
+
